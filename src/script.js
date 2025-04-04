8769bbba9a949d535ea36d24a9d7b9e122b5f5ab
Yassinproweb <yassinkatungi67@gmail.com>
2025-04-04 12:24:39 +0300

    correct fetch

--- src/script.js
+++ src/script.js
@@ -10,28 +10,28 @@ async function fetchData() {
     const div = document.createElement("div")
 
     div.innerHTML = `
-      <div class="relative w-full aspect-[4/2.85] flex-col items-center justify-center gap-5 rounded-2xl shadow-lg shadow-shade/10 overflow-hidden border border-shade/15 bg-white">
-        <h3 class="text-2xl text-green text-center font-bold">${dta.name}</h3>
-        <div class="div-banner absolute bottom-0 left-0 w-full flex items-center justify-between px-3.5 py-3 bg-white">
+      <div class="relative w-full aspect-[4/2.85] flex-col items-center justify-center gap-5 px-7 rounded-2xl shadow-md shadow-green/5 0 overflow-hidden border border-green/15 bg-white">
+        <h3 class="text-2xl text-shade text-center font-bold">${dta.name}</h3>
+        <div class="div-banner absolute bottom-0 left-0 w-full flex items-center justify-between px-3.5 py-3 bg-white text-shade">
           <div class="w-fit items-center justify-start gap-0.5">
-            <h4 class="text-6xl text-black font-bold">${dta.duruus}</h4>
-            <span class="w-fit flex flex-col items-center justify-center gap-0.5 text-black">
-              <i class="ph ph-file-audio text-3xl"></i>
-              <span class="text-sm">Duruus</span>
+            <h4 class="text-6xl font-bold">${dta.duruus}</h4>
+            <span class="w-fit flex flex-col items-center justify-center gap-0 text-green font-medium">
+              <i class="ph-fill ph-file-audio text-3xl"></i>
+              <span class="text-xs">Duruus</span>
             </span>
           </div>
           <div class="w-fit items-center justify-start gap-0.5">
-            <h4 class="text-6xl text-black font-bold">${dta.books}</h4>
-            <span class="w-fit flex flex-col items-center justify-center gap-0.5 text-black">
-              <i class="ph ph-books text-3xl"></i>
-              <span class="text-sm">Books</span>
+            <h4 class="text-6xl font-bold">${dta.books}</h4>
+            <span class="w-fit flex flex-col items-center justify-center gap-0 text-green font-medium">
+              <i class="ph-fill ph-books text-3xl"></i>
+              <span class="text-xs">Books</span>
             </span>
           </div>
           <div class="w-fit items-center justify-start gap-0.5">
-            <h4 class="text-6xl text-black font-bold">${dta.audios}</h4>
-            <span class="w-fit flex flex-col items-center justify-center gap-0.5 text-black">
-              <i class="ph ph-microphone text-3xl"></i>
-              <span class="text-sm">Audios</span>
+            <h4 class="text-6xl font-bold">${dta.audios}</h4>
+            <span class="w-fit flex flex-col items-center justify-center gap-0 text-green font-medium">
+              <i class="ph-fill ph-microphone text-3xl"></i>
+              <span class="text-xs">Audios</span>
             </span>
           </div>
         </div>
